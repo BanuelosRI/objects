b653f0ff60163a8bf17ecf8d1d28253e0a031c82
shiversamar <shiversamar@yahoo.com>
2016-09-29 16:39:37 -0700

    4thday of javascripts

---
+++ carobject.js
@@ -0,0 +1,185 @@
+//STORY 1
+//Purpose: 1.) Create a function that creates car objects which have a make, model year, and color.
+// 2.) Add a function that returns make, model year, and color as one string and use the method to tell the user the car's information.
+// 3.) Write a function that creates an object that represents a car with the following properties:
+// model year
+// make
+// color
+// 4.)Add a function to the object that is being returned that contatenates the make, year and color.
+//Signature: Object with function and method.
+// //Example: var person = {
+//   name: "Alex Keaton",
+//   phone: 123456789,
+//   getData: function() { return this.name+"-"+this.phone }
+// };
+//
+// person.getData();   -> "Alex Keaton-123456789"
+
+
+function newCar(modelyear, make, color) {
+  return {
+    modelyear: modelyear,
+    make: make,
+    color: color,
+    carInfo: function() {
+      return this.modelyear + "-" + this.make + "-" + this.color
+    }
+    };
+  }
+// To run this first function create a "var" with agrument  Example : var caro = newCar(1999, "BMW", "Blue");
+// Then you can call the var ( var.make) to get the make.
+// Or call the var (var.function name)to run the function that is inside the function.
+// caro.modelyear;, caro.make;, caro.color; ==>  will appear by modelyear,make,color in single
+
+
+//STORY 2
+//Purpose: 1.)The car can accelerate, brake, and tell you its speed.
+// 2.) Add a speed variable to the closure of the car object.
+// 3.)Add the following functions to the object:
+// getSpeed: returns current speed
+// accelerate: increases the speed by 10mph (but does not return anything)
+// brake: decreases the speed by 7mph (but does not return anything)
+// Signature :
+// Example:
+
+function newCar(modelyear, make, color) {
+  var speed = 10;
+  return {
+    modelyear: modelyear,
+    make: make,
+    color: color,
+    carInfo: function() {
+      return this.modelyear + "-" + this.make + "-" + this.color;
+    },
+    getSpeed: function() {
+      return speed;
+    },
+    accelerate: function() {
+      speed += 10;
+    },
+    brake: function() {
+      speed -= 7;
+    }
+    };
+  }
+
+// Story 3
+// create object, then a loop that takes car to and including 50 and no further. Then back to 0.
+//Signature : while loop
+
+function newCar(modelYear, Make, Color) {
+  var speed = 0;
+  return {
+    modelyear: modelYear,
+    make: Make,
+    color: Color,
+    carInfo: function() {
+      return this.modelyear + "-" + this.make + "-" + this.color;
+    },
+    getSpeed: function() {
+      return speed;
+    },
+    accelerate: function() {
+      speed += 5;
+    },
+    brake: function() {
+      speed -= 1;
+    },
+
+    };
+  }
+
+var car34 = newCar(1999, "Honda", "Blue");
+// this was possible becasue we had previously created the newCar creator object.
+while (car34.getSpeed() < 70) {
+  car34.accelerate();
+  console.log(car34.getSpeed());
+};
+
+while (car34.getSpeed() > Math.abs(0)) {
+  car34.brake();
+  console.log(car34.getSpeed());
+};
+
+//story 4
+//Purpose: Limit the car's maximum speed to 85mph.
+//Signature: If statement and add var for comparison
+//Example:
+
+
+function newCar(modelYear, Make, Color) {
+  var speed = 0;
+  var topSpeed = 85;
+  return {
+    modelyear: modelYear,
+    make: Make,
+    color: Color,
+    carInfo: function() {
+      return this.modelyear + "-" + this.make + "-" + this.color;
+    },
+    getSpeed: function() {
+      return speed;
+    },
+    accelerate: function() {
+      speed += 5;
+
+      if (speed >= topSpeed) {
+      speed = 85;
+      console.log("You are going to fast!");
+      }
+    },
+    brake: function() {
+      speed -= 1;
+    },
+
+    };
+  }
+
+var car34 = newCar(1999, "Honda", "Blue");
+
+  while (car34.getSpeed() < 85) {
+    car34.accelerate();
+    console.log(car34.getSpeed());
+  };
+
+// story 5
+// Purpose: make the brake change the speed by a random amount with a maximum of minus 1/2 of the current speed.
+
+
+function newCar(modelYear, Make, Color) {
+  var speed = 0;
+  var decSpeed;
+  var newSpeed;
+  var randNum = Math.floor((Math.random() * 10) + 1);
+  //will select arandom number between 1 - 10
+  return {
+    modelyear: modelYear,
+    make: Make,
+    color: Color,
+    carInfo: function() {
+      return this.modelyear + "-" + this.make + "-" + this.color;
+    },
+    getSpeed: function() {
+      return speed;
+    },
+    accelerate: function() {
+      speed += 5;
+    },
+    brake: function() {
+      newSpeed = speed + randNum;
+      decSpeed = (speed / 2);
+      return console.log(newSpeed);
+    },
+    brakeSpeed: function(){
+      speed = newSpeed - decSpeed;
+      return console.log(randNum, decSpeed);
+    }
+  };
+}
+
+var car34 = newCar(1999, "Honda", "Blue");
+
+while (car34.getSpeed() < 25) {
+  car34.accelerate();
+  console.log(car34.getSpeed());
+};
---
+++ password.js
@@ -0,0 +1,53 @@
+// Purpose: 1. Take your Password Checker and write in code that will notify the user when they have entered a non-valid user name and password.
+// 2. For each criteria create a function that checks the string for the password or username and returns one of two objects.
+//
+// 3.Either { valid: true } or { valid: false, reason: "User ID cannot contain $"} with the reason for the rule failing.
+// Then create a main function that runs all functions and prints out all failures for all criteria one at a time.
+// Signature :
+// Example :
+
+
+
+
+
+
+
+
+// checkId will take string, boolean
+// checkId is true if string is valid, if not it is false.
+// userID ("123456") would be true
+// userID "password" would be true
+// userID ("12345") would be false
+// userID ("123 ! # $") would be false
+
+
+var userID;
+var pass;
+
+function checkId(userID) {
+  return userID.length >= 6 && !userID.includes("!") && !userID.includes("#") && !userID.includes("$");
+}
+
+function checkPass(pass) {
+  return pass.length >= 6 && pass.toUpperCase() != pass && pass.toLowerCase() != pass && ((pass.includes("!") || pass.includes("#") || pass.includes("$")) && (pass.includes("0") || pass.includes("1") || pass.includes("2") || pass.includes("3") || pass.includes("4") || pass.includes("5") || pass.includes("6") || pass.includes("7") || pass.includes("8") || pass.includes("9")));
+}
+
+function checkCreds(userID, pass) {
+  return checkId(userID) && checkPass(pass) && pass!== userID;
+}
+
+function getCreds() {
+  userID = prompt("Enter userID. Can't be same as password. Must be atleast 6 characters. Cannot have !, #, or $");
+  pass = prompt("Enter password. Must be atleast 6 characters. Must include one of the following: !, #, or $. Cannot be 'password'");
+  alert("Your creds are acceptable " + checkCreds(userID, pass));
+}
+
+var
+function psCheck() {
+  return{
+    checkId: function() {
+      userID.length >= 6 && !userID.includes("!") && !userID.includes("#") && !userID.includes("$");
+    },
+
+};
+}
